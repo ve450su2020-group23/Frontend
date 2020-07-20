cc35424335e110197a42a49bdf2f44f2a9484290
kzlkz <larryli@umich.edu>
2020-07-20 15:50:19 -0400

    Fonts issue

--- src/index.js
+++ src/index.js
@@ -2,11 +2,89 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./static/css/index.css";
 import App from "./App";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+import CssBaseline from "@material-ui/core/CssBaseline";
+
 import * as serviceWorker from "./serviceWorker";
+
+import varelaTtf from "./static/fonts/Varela-Regular.ttf";
+import IBMPlexTtf from "./static/fonts/IBMPlexSerif-Regular.ttf";
+import vollkornTtf from "./static/fonts/Vollkorn-Regular.ttf";
+import alegreyaTtf from "./static/fonts/AlegreyaSans-Regular.ttf";
+import sourceSansProTtf from "./static/fonts/SourceSansPro-Regular.ttf";
+
+const sourceSansPro = {
+  fontFamily: "sourceSansPro",
+  src: `
+  local('sourceSansPro'),
+  local('sourceSansPro'),
+  url(${sourceSansProTtf}) format('sourceSansPro')
+`,
+};
+
+const alegraya = {
+  fontFamily: "alegreya",
+  src: `
+  local('alegreya'),
+  local('alegreya'),
+  url(${alegreyaTtf}) format('alegraya')
+`,
+};
+
+const varela = {
+  fontFamily: "varela",
+  src: `
+  local('varela'),
+  local('varela'),
+  url(${varelaTtf}) format('varela')
+`,
+};
+
+const vollkorn = {
+  fontFamily: "vollkorn",
+  src: `
+  local('vollkorn'),
+  local('vollkorn'),
+  url(${vollkornTtf}) format('vollkorn')
+`,
+};
+
+const IBMPlex = {
+  fontFamily: "IBMPlex",
+  src: `
+  local('IBMPlex'),
+  local('IBMPlex'),
+  url(${IBMPlexTtf}) format('IBMPlex')
+`,
+};
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      "IBMPlex",
+      "alegreya",
+      "vollkorn",
+      "varela",
+      "Arial",
+      "sourceSansPro",
+    ].join(","),
+    overrides: {
+      MuiCssBaseline: {
+        "@global": {
+          "@font-face": [varela, IBMPlex, vollkorn, alegraya, sourceSansPro],
+        },
+      },
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
